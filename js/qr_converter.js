56575758e18b5a60ede32e4cd40d869260cb2086
ouchinao <ouchi.nao.jg@tut.jp>
2024-06-28 15:13:53 +0900

    pdfファイルを直接あげるのは静的サイトだと無理なので、urlから生成することにした

--- js/qr_converter.js
+++ js/qr_converter.js
@@ -1,41 +1,23 @@
-// URLを短縮する関数
-function shortenUrl(url) {
-    return fetch(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(url)}`)
-        .then(response => response.text())
-        .then(shortUrl => {
-            console.log('APIによって短縮されたURL:', shortUrl);
-            return shortUrl;
-        })
-        .catch(error => {
-            console.error('APIを使用したURL短縮中にエラーが発生しました:', error);
-            throw error;
-        });
-}
-
 document.getElementById('generateQRCode').addEventListener('click', () => {
-    const pdfInput = document.getElementById('pdfInput').files[0];
-    if (!pdfInput) {
-        alert('Please select a PDF file first.');
+    const urlInput = document.getElementById('urlInput').value;
+    if (!urlInput) {
+        alert('URLを入力してください。');
         return;
     }
 
-    const reader = new FileReader();
     reader.onload = async function(event) {
         const pdfDataUrl = event.target.result;
-        // URLを短縮する
-        const shortUrl = await shortenUrl(pdfDataUrl);
-        console.log(shortUrl)
         // QRコードを生成する
         const qr = new QRious({
             element: document.getElementById('qrCode'),
-            value: shortUrl,
+            value: pdfDataUrl,
             size: 250,
             level: 'H',
         });
 
         // データURIをリンクとして表示する
         const qrCodeLink = document.getElementById('qrCodeLink');
-        qrCodeLink.innerHTML = `Access your PDF <a href="${shortUrl}" target="_blank">here</a>`;
+        qrCodeLink.innerHTML = `Access your PDF <a href="${pdfDataUrl}" target="_blank">here</a>`;
     };
     
     reader.readAsDataURL(pdfInput);
